3a2c50ec15fb64c82a19795b4867986aa9846ce2
Yaroslav Himko <yaroslav.himko@gmail.com>
2021-05-02 17:37:55 +0300

    Initial commit

---
+++ SwiftCollectionViewCells/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftCollectionViewCells
+//
+//  Created by Yaroslav Himko on 2.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
